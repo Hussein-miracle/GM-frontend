b7004ec00ab32ead56c03a335f83cb869af07512
Abdullahi <miraacle64@gmail.com>
2022-11-12 16:24:44 +0100

    feat: added react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from './reduxtoolkit/app/store';
+import { store } from "./reduxtoolkit/app/store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,9 +12,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
